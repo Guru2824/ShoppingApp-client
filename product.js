2cab2b661f2bd486e140cf5a18ff377490a5c06e
Guru2824 <78194137+Guru2824@users.noreply.github.com>
2021-02-04 12:55:05 +0530

    Shopping-App

--- product.js
+++ product.js
@@ -1,14 +1,21 @@
 const { ProductAPI } = require("./productApi")
-let filters = { brandName: ["Apple", "Samsung", "RealMe"], ram: [3], price: { min: 10000, max: 50000 } };
+let filters = { brandName: ["Apple", "Samsung", "RealMe"], ram: [4.5], price: { min: 10000, max: 50000 } };
 
 const productsapi = new ProductAPI();
 productsapi.getAllProducts().then(res => {
-    console.log(res);
+    // console.log(res.data)
+    return res.data;
 });
 
 productsapi.searchProducts(filters).then(data => {
-    console.table(data)
+    // console.table(data)
+    return data;
 });
+
+productsapi.getAllBrandName().then(res => {
+    // console.log(res.data)
+    return res.data;
+})
 
 // let results = await productsapi.searchProducts(filters)
 
--- productApi.js
+++ productApi.js
@@ -9,7 +9,23 @@ class ProductAPI {
         return axios.get(url);
     }
 
+    getAllBrandName(){
+        const url = "https://shoppingapp-mock.herokuapp.com/api/brands";
+        return axios.get(url)
+    }
+
+    sorting(data1, data2) {
+        if (data1.price < data2.price) {
+            return -1;
+        } else if (data1.price > data2.price) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     async searchProducts(filters) {
+
         let productsResults = await this.getAllProducts();
         let products = productsResults.data
 
@@ -24,6 +40,8 @@ class ProductAPI {
             results = products.filter(p => !filters.hasOwnProperty("brandName") || brandName_str.length == 0 || brandName_str.includes(p.brandName));
             results = results.filter(p => !filters.hasOwnProperty("ram") || ram_str.length == 0 || ram_str.includes(p.ram));
             results = results.filter(p => !filters.hasOwnProperty("price") || p.price >= price_str.min && p.price <= price_str.max);
+
+            results.sort(this.sorting)
 
             return results;
 
